fb210d975d74f66906a94450849c69233d8c9743
Максим <max@Macbook--Max.local>
2025-07-30 00:56:39 +0300

    add templates

--- hash/hash_map/hash_map.hpp
+++ hash/hash_map/hash_map.hpp
@@ -18,9 +18,9 @@ maintain efficient performance.
 */
 
 #include <cstdio>
+#include <functional>
 
-using Long = int64_t;
-
+template <typename T, typename Y>
 class HashMap {
  public:
   static constexpr size_t cDefaultCapacity = 16;
@@ -29,22 +29,22 @@ class HashMap {
   HashMap(size_t cap = cDefaultCapacity, size_t size = 0);
   ~HashMap();
 
-  bool GetValByKey(Long key, Long& val) const;
-  void Insert(Long key, Long val);
-  void Erase(Long key);
+  bool GetValByKey(T key, Y& val) const;
+  void Insert(T key, Y val);
+  void Erase(T key);
 
  private:
   struct HashNode {
-    HashNode(Long key, Long val, HashNode* next);
+    HashNode(T key, Y val, HashNode* next);
 
     HashNode* GetNext() const { return next; }
-    Long GetKey() const { return key; }
-    Long GetVal() const { return val; }
-    void SetNext(HashNode* next) { HashNode::next = next; }
-    void SetVal(Long val) { HashNode::val = val; }
+    T GetKey() const { return key; }
+    Y GetVal() const { return val; }
+    void SetNext(HashNode* next) { this->next = next; }
+    void SetVal(Y val) { this->val = val; }
 
-    Long key;
-    Long val;
+    T key;
+    Y val;
     HashNode* next;
   };
 
@@ -56,6 +56,142 @@ class HashMap {
   double GetLoadFactor() const;
   void Rehash();
 
-  static Long HashFunction(Long key, Long cap);
-  Long HashFunction(Long key) const;
+  static size_t HashFunction(T key, size_t cap);
+  size_t HashFunction(T key) const;
 };
+
+template <typename T, typename Y>
+HashMap<T, Y>::HashMap(size_t cap, size_t size) : size_(size), cap_(cap) {
+  map_ = InitializeHashTable(cap_);
+}
+
+template <typename T, typename Y>
+typename HashMap<T, Y>::HashNode** HashMap<T, Y>::InitializeHashTable(
+    size_t cap) {
+  HashNode** map = new HashNode*[cap];
+  for (size_t i = 0; i < cap; ++i) {
+    map[i] = nullptr;
+  }
+  return map;
+}
+
+template <typename T, typename Y>
+HashMap<T, Y>::~HashMap() {
+  for (size_t i = 0; i < cap_; ++i) {
+    HashNode* curr = map_[i];
+    while (curr != nullptr) {
+      HashNode* next = curr->GetNext();
+      delete curr;
+      curr = next;
+    }
+    map_[i] = nullptr;
+  }
+  delete[] map_;
+}
+
+template <typename T, typename Y>
+bool HashMap<T, Y>::GetValByKey(T key, Y& val) const {
+  size_t bucket_idx = HashFunction(key);
+  HashNode* curr = map_[bucket_idx];
+
+  while (curr != nullptr) {
+    if (curr->GetKey() == key) {
+      val = curr->GetVal();
+      return true;
+    }
+    curr = curr->GetNext();
+  }
+  return false;
+}
+
+template <typename T, typename Y>
+void HashMap<T, Y>::Insert(T key, Y val) {
+  size_t bucket_idx = HashFunction(key);
+
+  HashNode* curr = map_[bucket_idx];
+  while (curr != nullptr) {
+    if (curr->GetKey() == key) {
+      curr->SetVal(val);
+      return;
+    }
+    curr = curr->GetNext();
+  }
+
+  HashNode* new_head = new HashNode(key, val, map_[bucket_idx]);
+  map_[bucket_idx] = new_head;
+  ++size_;
+
+  Rehash();
+}
+
+template <typename T, typename Y>
+void HashMap<T, Y>::Erase(T key) {
+  size_t bucket_idx = HashFunction(key);
+  HashNode* curr = map_[bucket_idx];
+  HashNode* prev = nullptr;
+
+  while (curr != nullptr) {
+    if (curr->GetKey() == key) {
+      if (prev == nullptr) {
+        map_[bucket_idx] = curr->GetNext();
+      } else {
+        prev->SetNext(curr->GetNext());
+      }
+      delete curr;
+      --size_;
+      return;
+    }
+    prev = curr;
+    curr = curr->GetNext();
+  }
+}
+
+template <typename T, typename Y>
+HashMap<T, Y>::HashNode::HashNode(T key, Y val, HashNode* next)
+    : key(key), val(val), next(next) {}
+
+template <typename T, typename Y>
+double HashMap<T, Y>::GetLoadFactor() const {
+  return static_cast<double>(size_) / cap_;
+}
+
+template <typename T, typename Y>
+void HashMap<T, Y>::Rehash() {
+  if (GetLoadFactor() <= cMaxLoad) {
+    return;
+  }
+
+  size_t new_cap = cap_ * 2;
+  HashNode** new_map = InitializeHashTable(new_cap);
+
+  for (size_t i = 0; i < cap_; ++i) {
+    HashNode* curr = map_[i];
+    while (curr != nullptr) {
+      T key = curr->GetKey();
+      Y val = curr->GetVal();
+      size_t new_bucket_idx = HashFunction(key, new_cap);
+
+      HashNode* new_node = new HashNode(key, val, new_map[new_bucket_idx]);
+      new_map[new_bucket_idx] = new_node;
+
+      HashNode* old_node = curr;
+      curr = curr->GetNext();
+      delete old_node;
+    }
+  }
+
+  delete[] map_;
+  map_ = new_map;
+  cap_ = new_cap;
+}
+
+template <typename T, typename Y>
+size_t HashMap<T, Y>::HashFunction(T key, size_t cap) {
+  std::hash<T> hasher;
+  return hasher(key) % cap;
+}
+
+template <typename T, typename Y>
+size_t HashMap<T, Y>::HashFunction(T key) const {
+  return HashFunction(key, cap_);
+}
